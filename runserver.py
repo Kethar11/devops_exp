a9c7fcf863ad4f57adbe5105578f992a0b4d9882
Kethar <ketharnathsiva1196@gmail.com>
2025-01-22 18:27:53 +0100

    corrected space in runserver for to test cicd pipleine

--- runserver.py
+++ runserver.py
@@ -6,8 +6,7 @@ from os import environ
 from FlaskWebProject1 import app
 
 if __name__ == '__main__':
-    HOST =     
-    environ.get('SERVER_HOST', '0.0.0.0')
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
         PORT = int(environ.get('SERVER_PORT', '8000'))
     except ValueError:
